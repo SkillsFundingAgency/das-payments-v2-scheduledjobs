356b8afd74fc8a3191fd925676788f42e6060f1f
Pramod Lawate <pramod.lawate@FujitsuMicrosoftPracticeUK.onmicrosoft.com>
2025-01-02 16:22:34 +0000

    Add unit tests for ApprenticeshipDataTrigger class

---
+++ src/SFA.DAS.Payments.ScheduledJobs.V1.UnitTest/Functions/ApprenticeshipDataTriggerTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Moq;
+using SFA.DAS.Payments.ScheduledJobs.V1.Functions;
+using SFA.DAS.Payments.ScheduledJobs.V1.Services;
+
+namespace SFA.DAS.Payments.ScheduledJobs.V1.UnitTest.Functions
+{
+    [TestFixture]
+    public class ApprenticeshipDataTriggerTests
+    {
+        private Mock<ILogger<ApprenticeshipDataTrigger>> _mockLogger;
+        private Mock<IApprenticeshipDataService> _mockApprenticeshipDataService;
+        private ApprenticeshipDataTrigger _apprenticeshipDataTrigger;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockLogger = new Mock<ILogger<ApprenticeshipDataTrigger>>();
+            _mockApprenticeshipDataService = new Mock<IApprenticeshipDataService>();
+            _apprenticeshipDataTrigger = new ApprenticeshipDataTrigger(_mockLogger.Object, _mockApprenticeshipDataService.Object);
+        }
+
+        //[Test]
+        //public async Task TimerTriggerApprenticeshipsReferenceDataComparison_ShouldCallProcessComparison()
+        //{
+        //    // Arrange
+        //    var timerInfo = new TimerInfo(null, new ScheduleStatus(), false);
+
+        //    // Act
+        //    await _apprenticeshipDataTrigger.TimerTriggerApprenticeshipsReferenceDataComparison(timerInfo);
+
+        //    // Assert
+        //    _mockApprenticeshipDataService.Verify(service => service.ProcessComparison(), Times.Once);
+        //}
+
+        [Test]
+        public async Task HttpTriggerApprenticeshipsReferenceDataComparison_ShouldCallProcessComparison()
+        {
+            // Arrange
+            var httpRequest = new Mock<HttpRequest>();
+
+            // Act
+            await _apprenticeshipDataTrigger.HttpTriggerApprenticeshipsReferenceDataComparison(httpRequest.Object);
+
+            // Assert
+            _mockApprenticeshipDataService.Verify(service => service.ProcessComparison(), Times.Once);
+        }
+    }
+}
