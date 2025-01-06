c7270da1fbd0a5e94f0d04e54e41eb5ccd2e2b61
Pramod Lawate <pramod.lawate@FujitsuMicrosoftPracticeUK.onmicrosoft.com>
2025-01-06 11:09:39 +0000

    Remove ServiceBusOutput attribute from JobsToBeDeleted

--- src/SFA.DAS.Payments.ScheduledJobs.V1/DTOS/SubmissionJobsToBeDeletedBatch.cs
+++ src/SFA.DAS.Payments.ScheduledJobs.V1/DTOS/SubmissionJobsToBeDeletedBatch.cs
@@ -5,7 +5,6 @@ namespace SFA.DAS.Payments.ScheduledJobs.V1.DTOS
 {
     public class SubmissionJobsToBeDeletedBatch
     {
-        [ServiceBusOutput("%EarningAuditDataCleanUpQueue%", Connection = "ServiceBusConnectionString")]
         public SubmissionJobsToBeDeletedModel[] JobsToBeDeleted { get; set; }
     }
 }
