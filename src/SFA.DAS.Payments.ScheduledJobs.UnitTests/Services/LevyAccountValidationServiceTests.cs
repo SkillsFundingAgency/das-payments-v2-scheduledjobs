bd3a52a397f7d25adde59942227d7ba5686b7c96
Pramod Lawate <pramod.lawate@FujitsuMicrosoftPracticeUK.onmicrosoft.com>
2025-01-08 15:53:35 +0000

    Refactor tests and add in-memory database setup

--- src/SFA.DAS.Payments.ScheduledJobs.UnitTests/Services/LevyAccountValidationServiceTests.cs
+++ src/SFA.DAS.Payments.ScheduledJobs.UnitTests/Services/LevyAccountValidationServiceTests.cs
@@ -1,9 +1,11 @@
 ﻿using FluentValidation;
 using FluentValidation.Results;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
 using Moq;
 using SFA.DAS.Payments.Application.Infrastructure.Logging;
 using SFA.DAS.Payments.Application.Repositories;
+using SFA.DAS.Payments.Model.Core.Audit;
 using SFA.DAS.Payments.Model.Core.Entities;
 using SFA.DAS.Payments.ScheduledJobs.DTOS;
 using SFA.DAS.Payments.ScheduledJobs.Services;
@@ -15,7 +17,7 @@ namespace SFA.DAS.Payments.ScheduledJobs.UnitTest.Services
     public class LevyAccountValidationServiceTests
     {
         private Mock<IDasLevyAccountApiWrapper> _mockDasLevyAccountApiWrapper;
-        private Mock<IPaymentsDataContext> _mockPaymentsDataContext;
+        private IPaymentsDataContext _paymentsDataContext;
         private Mock<IValidator<CombinedLevyAccountsDto>> _mockValidator;
         private Mock<IPaymentLogger> _mockPaymentLogger;
         private LevyAccountValidationService _levyAccountValidationService;
@@ -24,19 +26,94 @@ namespace SFA.DAS.Payments.ScheduledJobs.UnitTest.Services
         public void SetUp()
         {
             _mockDasLevyAccountApiWrapper = new Mock<IDasLevyAccountApiWrapper>();
-            _mockPaymentsDataContext = new Mock<IPaymentsDataContext>();
             _mockValidator = new Mock<IValidator<CombinedLevyAccountsDto>>();
             _mockPaymentLogger = new Mock<IPaymentLogger>();
 
+            var options = new DbContextOptionsBuilder<PaymentsDataContext>()
+                .UseInMemoryDatabase(databaseName: "PaymentsDatabase")
+                .Options;
+
+            _paymentsDataContext = new PaymentsDataContext(options);
+
             _levyAccountValidationService = new LevyAccountValidationService(
                 _mockDasLevyAccountApiWrapper.Object,
-                _mockPaymentsDataContext.Object,
+               _paymentsDataContext,
                 _mockValidator.Object,
                 _mockPaymentLogger.Object);
         }
 
+        [TearDown]
+        public void TearDown()
+        {
+            var levyAccount = _paymentsDataContext.LevyAccount.FirstOrDefault(l => l.AccountId == 1);
+            if (levyAccount != null)
+            {
+                _paymentsDataContext.LevyAccount.Remove(levyAccount);
+                _paymentsDataContext.SaveChanges();
+            }
+        }
+
+        private void SeedLevyAccount()
+        {
+            _paymentsDataContext.LevyAccount.Add(new LevyAccountModel
+            {
+                AccountId = 1,
+                AccountName = "Test",
+                Balance = 0,
+                IsLevyPayer = true,
+                TransferAllowance = 1
+            });
+
+            _paymentsDataContext.SaveChanges();
+        }
+
         [Test]
-        public async Task Validate_ShouldLogDebugAndInfoMessages()
+        public async Task Validate_ShouldValidateAsync()
+        {
+            // Arrange
+            var dasLevyAccounts = new List<LevyAccountModel> { new LevyAccountModel() };
+            var paymentsLevyAccounts = new List<LevyAccountModel> { new LevyAccountModel() };
+
+            _mockDasLevyAccountApiWrapper.Setup(x => x.GetDasLevyAccountDetails())
+                .ReturnsAsync(dasLevyAccounts);
+
+            _mockValidator.Setup(x => x.ValidateAsync(It.IsAny<CombinedLevyAccountsDto>(), default))
+                .ReturnsAsync(new ValidationResult());
+
+            SeedLevyAccount();
+
+            // Act
+            await _levyAccountValidationService.Validate();
+
+            // Assert
+            _mockValidator.Verify(a => a.ValidateAsync(It.IsAny<CombinedLevyAccountsDto>(), default), Times.Once);
+        }
+
+        [Test]
+        public async Task Validate_ShouldCallGetDasLevyAccountDetails()
+        {
+            // Arrange
+            var dasLevyAccounts = new List<LevyAccountModel> { new LevyAccountModel() };
+            var paymentsLevyAccounts = new List<LevyAccountModel> { new LevyAccountModel() };
+
+            _mockDasLevyAccountApiWrapper.Setup(x => x.GetDasLevyAccountDetails())
+                .ReturnsAsync(dasLevyAccounts);
+
+            _mockValidator.Setup(x => x.ValidateAsync(It.IsAny<CombinedLevyAccountsDto>(), default))
+                .ReturnsAsync(new ValidationResult());
+
+            SeedLevyAccount();
+
+            // Act
+            await _levyAccountValidationService.Validate();
+
+            // Assert
+            _mockDasLevyAccountApiWrapper.Verify(a => a.GetDasLevyAccountDetails(), Times.Once);
+        }
+
+
+        [Test]
+        public async Task Validate_LevyAccountModelsShouldBeNull()
         {
             // Arrange
             var dasLevyAccounts = new List<LevyAccountModel> { new LevyAccountModel() };
@@ -53,7 +130,6 @@ namespace SFA.DAS.Payments.ScheduledJobs.UnitTest.Services
 
             // Assert
             _mockDasLevyAccountApiWrapper.Verify(a => a.GetDasLevyAccountDetails(), Times.Once);
-            _mockValidator.Verify(a => a.ValidateAsync(It.IsAny<CombinedLevyAccountsDto>(), default), Times.Once);
         }
     }
 }
