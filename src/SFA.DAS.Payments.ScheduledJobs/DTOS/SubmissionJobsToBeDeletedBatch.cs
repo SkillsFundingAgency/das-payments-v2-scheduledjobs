28590816238047a0a3943d9f1a036d91f7cd403c
Pramod Lawate <pramod.lawate@FujitsuMicrosoftPracticeUK.onmicrosoft.com>
2025-01-10 12:12:35 +0000

    delete the DTOS folder

--- src/SFA.DAS.Payments.ScheduledJobs/DTOS/SubmissionJobsToBeDeletedBatch.cs
+++
@@ -1,9 +0,0 @@
-﻿using SFA.DAS.Payments.Model.Core.Audit;
-
-namespace SFA.DAS.Payments.ScheduledJobs.Dtos
-{
-    public class SubmissionJobsToBeDeletedBatch
-    {
-        public SubmissionJobsToBeDeletedModel[] JobsToBeDeleted { get; set; }
-    }
-}
