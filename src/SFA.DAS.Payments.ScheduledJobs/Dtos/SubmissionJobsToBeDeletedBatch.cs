264ef6ea4b44a3b336a1ac01f9132aa1ea1ef0e3
Pramod Lawate <pramod.lawate@FujitsuMicrosoftPracticeUK.onmicrosoft.com>
2025-01-10 12:14:12 +0000

    Rename the Dtos folder

---
+++ src/SFA.DAS.Payments.ScheduledJobs/Dtos/SubmissionJobsToBeDeletedBatch.cs
@@ -0,0 +1,9 @@
+﻿using SFA.DAS.Payments.Model.Core.Audit;
+
+namespace SFA.DAS.Payments.ScheduledJobs.Dtos
+{
+    public class SubmissionJobsToBeDeletedBatch
+    {
+        public SubmissionJobsToBeDeletedModel[] JobsToBeDeleted { get; set; }
+    }
+}
