499b29aafbef5e7e707516e828f86b5d0b71b76a
Pramod Lawate <pramod.lawate@FujitsuMicrosoftPracticeUK.onmicrosoft.com>
2025-01-16 09:56:10 +0000

    Add new configuration namespace and classes, Review comments fixed

---
+++ src/SFA.DAS.Payments.ScheduledJobs/Configuration/IAppSettingsOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.DAS.Payments.ScheduledJobs.Configuration
+{
+    public interface IAppSettingsOptions
+    {
+        ConnectionStrings ConnectionStrings { get; set; }
+        bool IsEncrypted { get; set; }
+        Values Values { get; set; }
+    }
+}
