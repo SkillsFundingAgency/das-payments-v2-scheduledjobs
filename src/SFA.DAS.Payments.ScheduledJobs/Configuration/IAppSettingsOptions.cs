99c803b4cdfda3b0dd2c3ec3dd8aa4a0e2c51e76
Pramod Lawate <pramod.lawate@education.gov.uk>
2025-03-13 15:14:18 +0000

    Refactor configuration management and update packages #6
    Removed AppSettingsOptions and related classes/interfaces.
    Deleted the Welcome class and its HTTP handling logic.
    Updated various package references for improved functionality
    and compatibility, including Microsoft.ApplicationInsights
    and Azure.Messaging.ServiceBus.

--- src/SFA.DAS.Payments.ScheduledJobs/Configuration/IAppSettingsOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SFA.DAS.Payments.ScheduledJobs.Configuration
-{
-    public interface IAppSettingsOptions
-    {
-        ConnectionStrings ConnectionStrings { get; set; }
-        string IsEncrypted { get; set; }
-        Values Values { get; set; }
-    }
-}
